769f90b2932b29660368ede98d831a2f2c1d3fa3
GerardoNR <22620048@tlaxiaco.tecnm.mx>
2025-04-08 23:14:13 -0600

    creacion del carousel y bienvenida @jessica

--- cafeteria/src/App.vue
+++ cafeteria/src/App.vue
@@ -1,30 +1,60 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import Carousel from "./components/Carousel.vue";
+export default {
+  name: "App",
+  components: {
+    Carousel,
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app">
+    <header>
+      <h1>Bienvenidos a Café del Sol</h1>
+      <p>¡El mejor lugar para disfrutar un café y buena compañía!</p>
+    </header>
+   <!-- Navegador web  -->
+   <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="black">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+</nav>
+
+<Carousel />
+    <router-view />
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+header{
+  background-color: black;
+  color: #FFF;
+  text-align: center;
+  width: auto;
+  height: 100px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.app{
+  background-color: black;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
